9aaa37ce0075ea8b9c546c412eec0ac2669e5990
Felipe Gabriel <133790793+FelipeGabriel05@users.noreply.github.com>
2025-02-25 06:57:59 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
     Organização do Trabalho, Interface.c, Makefile, reformulação e criação de algumas funções do Jogo.c e Tabuleiro.c, validação e Debug das funções do programa.
   Marcos Vinicius Pereira Araujo - 571042:
   Felipe Gabriel Lima Florindo - 569760:
+    Makefile, reformulação e criação de algumas funções de jogo.c, tabuleiro.c e interface.c.
 */
 
 #include <stdio.h>
