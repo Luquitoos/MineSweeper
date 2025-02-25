83ac5a7b155db00a81792a2b45b6dec75e71cb35
Lucas Magalhães <lucasm.bizerril@gmail.com>
2025-02-25 12:53:10 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 /*Lucas Magalhães Fontenele Bizeril - 572968:
     Organização do Trabalho, Interface.c, Makefile, reformulação e criação de algumas funções do Jogo.c e Tabuleiro.c, validação e Debug das funções do programa.
   Marcos Vinicius Pereira Araujo - 571042:
+    Grande parte das funções do Jogo.c e Tabuleiro.c, foco na Estrutura de Dados
   Felipe Gabriel Lima Florindo - 569760:
     Makefile, reformulação e criação de algumas funções de jogo.c, tabuleiro.c e interface.c.
 */
